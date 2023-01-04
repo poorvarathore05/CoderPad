6d1099c40610e8d6bb2809382f8546e3ddc771d1
poorvarathore05 <poorvarathore05@gmail.com>
2023-01-04 15:43:45 -0800

    code modification

--- src/Pages/CodeEditor.js
+++ src/Pages/CodeEditor.js
@@ -1,27 +1,25 @@
 import { useEffect, useRef } from "react";
 import "./Editor.css";
-import CodeMirror from "react-codemirror";
+import CodeMirror from "codemirror";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/monokai.css";
-import "codemirror/mode/javascript/javascript.js";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
 import ACTIONS from "../ACTIONS";
 
 function CodeEditor({ socketRef, roomId, onCodeChange }) {
   const editorRef = useRef(null);
 
   useEffect(() => {
-    if (socketRef.current) {
-      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
-        if (code !== null) {
-          editorRef.current.codeMirror.setValue(code);
-        }
-      });
-    }
-  }, [socketRef.current]);
-
-  useEffect(() => {
     async function init() {
-      editorRef.current.codeMirror.on("change", (instance, changes) => {
+      editorRef.current = CodeMirror.fromTextArea(
+        document.getElementById("textEditor"),
+        {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          lineNumbers: true,
+        }
+      );
+      editorRef.current.on("change", (instance, changes) => {
         const { origin } = changes;
         const code = instance.getValue();
         onCodeChange(code);
@@ -36,13 +34,20 @@ function CodeEditor({ socketRef, roomId, onCodeChange }) {
     init();
   }, []);
 
-  const options = {
-    lineNumbers: true,
-    mode: "javascript",
-    theme: "monokai",
-  };
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+        }
+      });
+    }
+    return () => {
+      socketRef.current.off(ACTIONS.CODE_CHANGE);
+    };
+  }, [socketRef.current]);
 
-  return <CodeMirror ref={editorRef} options={options} />;
+  return <textarea id="textEditor"></textarea>;
 }
 
 export default CodeEditor;
